87655f0f60555d3c78f964389cfcee7e2faebba7
Mike Weiblen <mew@mew.cx>
2022-05-22 18:29:29 -0600

    code.py

--- code.py
+++ code.py
@@ -1,1 +1,6 @@
-import kewbowcode
+print("dotstar")
+
+import status_led_one_dotstar_rainbow
+#import status_led_one_dotstar_rgb
+#import status_multi_dotstar_rainbow
+#import status_multi_dotstar_rgb
