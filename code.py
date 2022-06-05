3c09dbdf84742c491f404045afb07b4aa84bae46
Mike Weiblen <mew@mew.cx>
2022-06-04 19:25:59 -0600

    code.py

--- code.py
+++ code.py
@@ -1,6 +1,9 @@
 print("dotstar")
 
-import status_led_one_dotstar_rainbow
+import dotstar_simpletest
+#import status_led_one_dotstar_rainbow
 #import status_led_one_dotstar_rgb
 #import status_multi_dotstar_rainbow
 #import status_multi_dotstar_rgb
+
+# eof
