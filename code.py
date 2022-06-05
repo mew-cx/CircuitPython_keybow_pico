2b42bf2802a1c0bb0d5386e70ae949bfa492b59e
Mike Weiblen <mew@mew.cx>
2022-06-04 19:28:11 -0600

    create dust_dotstar.py

--- code.py
+++ code.py
@@ -1,6 +1,8 @@
 print("dotstar")
 
-import dotstar_simpletest
+import dust_dotstar
+
+#import dotstar_simpletest
 #import status_led_one_dotstar_rainbow
 #import status_led_one_dotstar_rgb
 #import status_multi_dotstar_rainbow
