957b9891691bc42e7e619c64d8196f84a84c9614
Mike Weiblen <mew@mew.cx>
2022-06-05 01:57:37 -0600

    simple reformatting

--- kewbowcode.py
+++ kewbowcode.py
@@ -1,3 +1,5 @@
+# kewbowcode.py
+
 import board
 import digitalio
 import adafruit_dotstar
@@ -12,61 +14,59 @@ from adafruit_hid.keycode import Keycode
 from adafruit_hid.consumer_control import ConsumerControl
 from adafruit_hid.consumer_control_code import ConsumerControlCode
 
-pixels=adafruit_dotstar.DotStar(board.GP2, board.GP3,12)
+pixels = adafruit_dotstar.DotStar(board.GP2, board.GP3,12)
 kbd = Keyboard(usb_hid.devices)
-# we're americans :)
 layout = KeyboardLayoutUS(kbd)
 
 class KeyLight:
     def __init__(self,keynum,pinnum,lednum,press_string,press_func):
-        self.keynum=keynum
-        self.pinnum=pinnum
-        self.lednum=lednum
-        self.press_string=press_string
-        self.press_func=press_func
-        self.pressed=False
+        self.keynum = keynum
+        self.pinnum = pinnum
+        self.lednum = lednum
+        self.press_string = press_string
+        self.press_func = press_func
+        self.pressed = False
         self.set_base_color((self.keynum*20,0,255-(self.keynum*20) , 0.5))
         self.activate_pin()
 
     def activate_pin(self):
-        self.pin=digitalio.DigitalInOut(self.pinnum)
-        self.pin.switch_to_input(pull=digitalio.Pull.UP)
+        self.pin = digitalio.DigitalInOut(self.pinnum)
+        self.pin.switch_to_input(pull = digitalio.Pull.UP)
 
     def set_pixel(self,color):
-        pixels[self.lednum]=color
+        pixels[self.lednum] = color
 
     def get_pixel(self):
         return pixels[self.lednum]
 
     def set_base_color(self,color):
-        self.base_color=color
+        self.base_color = color
         self.set_pixel(color)
 
     def key_pressed(self):
-        self.pressed=True
+        self.pressed = True
 
     def key_was_pressed(self):
         return self.pressed
 
     def key_released(self):
-        self.pressed=False
+        self.pressed = False
 
     def key_down(self):
-        return self.pin.value==False
+        return self.pin.value == False
 
-
-macro_n=0
+macro_n = 0
 
 def key_macroinckey(kdb,layout):
     global macro_n
-    layout.write(f"Macro {macro_n}")  
+    layout.write(f"Macro {macro_n}")
     macro_n += 1
 
 # def key_undo(kdb,layout):
 #     kbd.press(Keycode.COMMAND,Keycode.Z)
 #     kbd.release_all()
 
-keyArray=[]
+keyArray = []
 keyArray.append( KeyLight( 0, board.GP7,   3,  None, key_macroinckey))
 keyArray.append( KeyLight( 1, board.GP8,   7,  "1",  None))
 keyArray.append( KeyLight( 2, board.GP27,  11, "2",  None))
@@ -80,15 +80,14 @@ keyArray.append( KeyLight( 9, board.GP16,  0,  "9",  None))
 keyArray.append( KeyLight( 10, board.GP17, 4,  "Ten", None))
 keyArray.append( KeyLight( 11, board.GP14, 8,  "Eleven", None))
 
-
 while True:
-    keydown=False
+    keydown = False
     for k in keyArray:
         if k.key_down():
-            if not k.key_was_pressed(): 
+            if not k.key_was_pressed():
                 k.set_pixel((255,255,255,0.5))
                 k.key_pressed()
-                if k.press_func!=None:
+                if k.press_func:
                     k.press_func(kbd,layout)
                 else:
                     layout.write(k.press_string)
@@ -97,5 +96,3 @@ while True:
         elif k.key_was_pressed():
                 k.key_released()
                 k.set_pixel(k.base_color)
-
-
