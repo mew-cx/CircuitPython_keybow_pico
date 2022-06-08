a29903fcd9895a26fb7b89de0234e9695e9cc5c9
Mike Weiblen <mew@mew.cx>
2022-06-08 03:21:24 -0600

    rm unneeded from dotstar merge

--- code.py
+++ code.py
@@ -2,10 +2,4 @@ print("dotstar")
 
 import dust_dotstar
 
-#import dotstar_simpletest
-#import status_led_one_dotstar_rainbow
-#import status_led_one_dotstar_rgb
-#import status_multi_dotstar_rainbow
-#import status_multi_dotstar_rgb
-
 # eof
