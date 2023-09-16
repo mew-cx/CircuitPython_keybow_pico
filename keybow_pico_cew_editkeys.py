a3f1a957aed913eb611072adf10abe9b29b382f6
Mike Weiblen <mew@mew.cx>
2023-09-15 23:58:15 -0600

    keybow_pico_cew_editkeys.py 0.2.5.5

--- keybow_pico_cew_editkeys.py
+++ keybow_pico_cew_editkeys.py
@@ -2,73 +2,98 @@
 #
 # SPDX-License-Identifier: MIT
 
-# keybow_pico_cew_editkeys.py -- http://mew.cx/ 2023-02-24
-# A configuration of the 12-key "Keybow Pico" for CEW's editing shortcuts.
+# mew-cx/CircuitPython_keybow_pico/keybow_pico_cew_editkeys.py -- http://mew.cx 2023-09-16
+#
+# The "keybow_pico" is a Pimoroni 12-key Keybow which replaces the original
+# RaspberryPi Zero with a RaspberryPi Pico using a Red Robotics Pico2Pi
+# adapter board.
+#
+# A configuration of the 12-key "Keybow Pico" with CEW's editing shortcuts.
+# (For my lovely Cerrise, to help her doctoral research)
 
-#import time
 import board
 import keypad
-import adafruit_dotstar
 import atexit
 import usb_hid
+from adafruit_dotstar import DotStar
 from adafruit_hid.keycode import Keycode
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.mouse import Mouse
 
-__version__ = "0.2.0.0"
-__repo__ = "https://github.com/mew-cx/CircuitPython_keybow_pico"
+__version__ = "0.2.5.5"
+__repo__ = "https://github.com/mew-cx/CircuitPython_keybow_pico.git"
+__board_id__ = "raspberry_pi_pico"    # board.board_id
+__impl_name__ = "circuitpython"       # sys.implementation.name
+__impl_version__ = (8, 2, 6)          # sys.implementation.version
 
 # Output devices to receive events from this program.
 keybd = Keyboard(usb_hid.devices)
 mouse = Mouse(usb_hid.devices)
-leds  = adafruit_dotstar.DotStar(board.GP2, board.GP3, 12, brightness=0.5)
+leds  = DotStar(clock=board.GP2, data=board.GP3, n=12, brightness=0.7)
 
-# Elements of this table are in Keybow's key/LED order (0-11).
+# keybow_pico key/LED layout (0-11):
+#  +-----------+
+#  |     KEYBOW|
+#  | 0   4   8 |
+#  |           |
+#  | 1   5   9 |
+#  |           |
+#  | 2   6  10 |
+#  |           |
+#  | 3   7  11 |
+#  +-----------+
+#    USB
+
 MODIFIER = Keycode.CONTROL
-KEYINFO = (
-    # GPIO pin     color      pressfunc     relfunc         eventdata                keynumber
-    ( board.GP16,  0xff00ff,  keybd.press,  keybd.release,  [MODIFIER, Keycode.Z]),  #  0
-    ( board.GP11,  0x0000ff,  keybd.press,  keybd.release,  [MODIFIER, Keycode.C]),  #  1
-    ( board.GP9,   0x0000ff,  keybd.press,  keybd.release,  [MODIFIER, Keycode.V]),  #  2
-    ( board.GP7,   0x0000ff,  keybd.press,  keybd.release,  [MODIFIER, Keycode.A]),  #  3
-    ( board.GP17,  0xffff00,  keybd.press,  keybd.release,  [Keycode.BACKSPACE]),    #  4
-    ( board.GP18,  0x777777,  mouse.press,  mouse.release,  [Mouse.RIGHT_BUTTON]),   #  5
-    ( board.GP26,  0xff0000,  keybd.press,  keybd.release,  [Keycode.UP_ARROW]),     #  6
-    ( board.GP8,   0x777777,  mouse.press,  mouse.release,  [Mouse.LEFT_BUTTON]),    #  7
-    ( board.GP14,  0x00ff00,  keybd.press,  keybd.release,  [Keycode.ENTER]),        #  8
-    ( board.GP12,  0xff0000,  keybd.press,  keybd.release,  [Keycode.RIGHT_ARROW]),  #  9
-    ( board.GP10,  0xff0000,  keybd.press,  keybd.release,  [Keycode.DOWN_ARROW]),   # 10
-    ( board.GP27,  0xff0000,  keybd.press,  keybd.release,  [Keycode.LEFT_ARROW])    # 11
+KEYDATA = (
+    # Table of events to be generated for each key press.
+    # Elements of this table must be in Keybow's key/LED order.
+    # 0:GPIO pin   1:color    2:pressfunc   3:relfunc       4:eventdata              key/LED#
+    ( board.GP16,  0x770077,  keybd.press,  keybd.release,  [MODIFIER, Keycode.Z] ),  #  0
+    ( board.GP11,  0x0000ff,  keybd.press,  keybd.release,  [MODIFIER, Keycode.C] ),  #  1  kittykey!
+    ( board.GP9,   0x000077,  keybd.press,  keybd.release,  [MODIFIER, Keycode.V] ),  #  2
+    ( board.GP7,   0x000077,  keybd.press,  keybd.release,  [MODIFIER, Keycode.A] ),  #  3
+    ( board.GP17,  0x777700,  keybd.press,  keybd.release,  [Keycode.BACKSPACE] ),    #  4
+    ( board.GP18,  0x777777,  mouse.press,  mouse.release,  [Mouse.RIGHT_BUTTON] ),   #  5
+    ( board.GP26,  0x770000,  keybd.press,  keybd.release,  [Keycode.UP_ARROW] ),     #  6
+    ( board.GP8,   0x777777,  mouse.press,  mouse.release,  [Mouse.LEFT_BUTTON] ),    #  7
+    ( board.GP14,  0x007700,  keybd.press,  keybd.release,  [Keycode.ENTER] ),        #  8
+    ( board.GP12,  0x770000,  keybd.press,  keybd.release,  [Keycode.RIGHT_ARROW] ),  #  9
+    ( board.GP10,  0x770000,  keybd.press,  keybd.release,  [Keycode.DOWN_ARROW] ),   # 10
+    ( board.GP27,  0x770000,  keybd.press,  keybd.release,  [Keycode.LEFT_ARROW] )    # 11
 )
 
-# At program termination, shutdown cleanly: LEDs off, keys/buttons released.
 @atexit.register
 def shutdown():
+    """
+    At program termination, shutdown cleanly:
+    LEDs off, keys/buttons released.
+    """
     leds.fill(0)
     keybd.release_all()
     mouse.release_all()
 
+# The main program ##########################################################
+
 def main():
-    # Assign GPIO pins to keypad keys, and scan @ 50Hz (20mS) for debouncing.
-    keys = keypad.Keys([i[0] for i in KEYINFO], interval=0.02, value_when_pressed=False, pull=True)
+    """Convert keypad events to USB keyboard/mouse events."""
+    # Configure GPIO pins for the keys; scan at 20mS (50Hz) for debouncing.
+    keys = keypad.Keys([kdata[0] for kdata in KEYDATA],
+        interval=0.020, value_when_pressed=False, pull=True)
 
-    # Set the keypad's LED colors
+    # Initialize all keys' LED colors
     leds.fill(0)
-    for knum,kinfo in enumerate(KEYINFO):
-        leds[knum] = kinfo[1]
+    for i,kdata in enumerate(KEYDATA):
+        leds[i] = kdata[1]
 
+    # Process key events forever
     while True:
         event = keys.events.get()
-        if event:
-            kinfo     = KEYINFO[event.key_number]
-            pressfunc = kinfo[2]
-            relfunc   = kinfo[3]
-            eventdata = kinfo[4]
+        if event is not None:
+            _, _, pressfunc, relfunc, eventdata = KEYDATA[event.key_number]
             if event.pressed:
                 pressfunc(*eventdata)
             else:
                 relfunc(*eventdata)
-
-main()
 
 # vim: set sw=4 ts=8 et ic ai:
